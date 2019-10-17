cd131207bd9f02d39f2944a5f5d29699436b43b8
Anthony <anthonyzhu132@gmail.com>
2019-10-17 20:41:03 +0000

    added dashboard information and functionality

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["NAME"], password: ENV["PASSWORD"], except: :index
   def show
+    @products = Product.count
+    @categories = Category.count
   end
 end
